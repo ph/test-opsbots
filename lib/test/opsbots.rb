2e3b4550471a434f7c50dbec7c07d58ab8b09ff7
Pier-Hugues Pellerin <phpellerin@gmail.com>
2014-11-24 14:58:47 -0500

    test

---
+++ lib/test/opsbots.rb
@@ -0,0 +1,7 @@
+require "test/opsbots/version"
+
+module Test
+  module Opsbots
+    # Your code goes here...
+  end
+end
---
+++ lib/test/opsbots/version.rb
@@ -0,0 +1,5 @@
+module Test
+  module Opsbots
+    VERSION = "0.0.1"
+  end
+end
